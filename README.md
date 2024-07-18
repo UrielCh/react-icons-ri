f0706f8caf0f30386c9f2524f889f5c0736283db
urielch <uriel.chemouni@gmail.com>
2024-07-18 10:53:35 +0200

    update doc

--- README.md
+++ README.md
@@ -8,7 +8,7 @@
 
 [See available icons here](https://react-icons.deno.dev/ri)
 
-## install packahe
+## install the module
 
 ```bash
 deno add @preact-icons/ri
@@ -17,36 +17,15 @@ pnpm dlx jsr add @preact-icons/ri
 bunx jsr add @preact-icons/ri
 ```
 
-You may need to update your preact mapping to:
- - "preact": "npm:preact@10.22.1",
- - "preact/jsx-runtime": "npm:preact@10.22.1/jsx-runtime",
- - "preact/hooks": "npm:preact@10.22.1/hooks",
-
-
-# Remix Icon icons for preact
-
-[![JSR](https://jsr.io/badges/@preact-icons/ri)](https://jsr.io/@preact-icons/ri)
-
-**License** [Apache License Version 2.0](http://www.apache.org/licenses/)
-
-**Project** [https://github.com/Remix-Design/RemixIcon](https://github.com/Remix-Design/RemixIcon)
-
-[See available icons here](https://react-icons.deno.dev/ri)
-
-## install packahe
-
-```bash
-deno add @preact-icons/ri
-dnpx jsr add @preact-icons/ri
-pnpm dlx jsr add @preact-icons/ri
-bunx jsr add @preact-icons/ri
+You may need to update your preact mapping to avoid mixing JSR and http import:
+Currently Deno fresh import preact using https://esm.sh/preact http import can not be mixed with JSR package, so you may need to update your preact mapping:
+```json
+{
+ "preact": "npm:preact@10.22.1",
+ "preact/jsx-runtime": "npm:preact@10.22.1/jsx-runtime",
+ "preact/hooks": "npm:preact@10.22.1/hooks",
+}
 ```
-
-You may need to update your preact mapping to:
- - "preact": "npm:preact@10.22.1",
- - "preact/jsx-runtime": "npm:preact@10.22.1/jsx-runtime",
- - "preact/hooks": "npm:preact@10.22.1/hooks",
-
 
 ## import an icon from all icons
 
@@ -65,4 +44,3 @@ or using default export
 ```ts
 import RiArrowDownCircleFill2 from "react-icons/ri/RiArrowDownCircleFill2.ts"
 ```
-
--- mod.ts
+++ mod.ts
@@ -10,7 +10,7 @@
  * 
  * [See available icons here](https://react-icons.deno.dev/ri)
  * 
- * ## install packahe
+ * ## install the module
  * 
  * ```bash
  * deno add @preact-icons/ri
@@ -19,36 +19,15 @@
  * bunx jsr add @preact-icons/ri
  * ```
  * 
- * You may need to update your preact mapping to:
- *  - "preact": "npm:preact@10.22.1",
- *  - "preact/jsx-runtime": "npm:preact@10.22.1/jsx-runtime",
- *  - "preact/hooks": "npm:preact@10.22.1/hooks",
- * 
- * 
- * # Remix Icon icons for preact
- * 
- * [![JSR](https://jsr.io/badges/@preact-icons/ri)](https://jsr.io/@preact-icons/ri)
- * 
- * **License** [Apache License Version 2.0](http://www.apache.org/licenses/)
- * 
- * **Project** [https://github.com/Remix-Design/RemixIcon](https://github.com/Remix-Design/RemixIcon)
- * 
- * [See available icons here](https://react-icons.deno.dev/ri)
- * 
- * ## install packahe
- * 
- * ```bash
- * deno add @preact-icons/ri
- * dnpx jsr add @preact-icons/ri
- * pnpm dlx jsr add @preact-icons/ri
- * bunx jsr add @preact-icons/ri
+ * You may need to update your preact mapping to avoid mixing JSR and http import:
+ * Currently Deno fresh import preact using https://esm.sh/preact http import can not be mixed with JSR package, so you may need to update your preact mapping:
+ * ```json
+ * {
+ *  "preact": "npm:preact@10.22.1",
+ *  "preact/jsx-runtime": "npm:preact@10.22.1/jsx-runtime",
+ *  "preact/hooks": "npm:preact@10.22.1/hooks",
+ * }
  * ```
- * 
- * You may need to update your preact mapping to:
- *  - "preact": "npm:preact@10.22.1",
- *  - "preact/jsx-runtime": "npm:preact@10.22.1/jsx-runtime",
- *  - "preact/hooks": "npm:preact@10.22.1/hooks",
- * 
  * 
  * ## import an icon from all icons
  * 
@@ -67,8 +46,8 @@
  * ```ts
  * import RiArrowDownCircleFill2 from "react-icons/ri/RiArrowDownCircleFill2.ts"
  * ```
- * 
  * @module
+ * 
  */
 
 import { GenIcon } from "./deps.ts";
